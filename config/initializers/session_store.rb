e1749b8aa9e909f926bb87bb0ee883cf081b6b62
Jethro Williams <jethrowilliams@outlook.com>
2022-01-28 16:32:42 +0000

    Update cors/session store to generic values

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,8 @@
+# TODO - Update both 'key' values below to your app name
+# TODO - Update the 'domain' value below to your API URL
+
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: "_wheres_jethro", domain: "api.wheresjethro.com" # TODO - Update this to whatever the API URL is
+  Rails.application.config.session_store :cookie_store, key: "_my_app_name", domain: "api.myappname.com"
 else
-  Rails.application.config.session_store :cookie_store, key: "_wheres_jethro"
+  Rails.application.config.session_store :cookie_store, key: "_my_app_name"
 end
